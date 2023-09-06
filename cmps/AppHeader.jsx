227b39475888566b933a61a860b05c8e7eee0b26
galbarcessat <galbarcessat1@gmail.com>
2023-09-06 11:39:38 +0300

    added fonts

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -4,7 +4,7 @@ export function AppHeader() {
   return (
     <header className="app-header">
       <Link to="/">
-        <h3><i class="fa-solid fa-horse-head"></i>AppSus</h3>
+        <h3 className="app-header-title"><i className="fa-solid fa-horse-head"></i>AppSus</h3>
       </Link>
       <nav>
         <NavLink to="/">Home</NavLink>
